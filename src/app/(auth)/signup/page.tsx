2d6d2a9f4c48b040148466107b7d197fd9723a4f
xyugen <alex.arias.jorym@gmail.com>
2024-11-14 20:53:37 +0800

    refactor(auth): :recycle: divide auth pages into reusable components
    includes the or divider, social auths, and hero section
    also created svg icon assets

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,9 +2,11 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { PageRoutes } from "@/constants/page-routes";
-import { Facebook, User } from "lucide-react";
-import Image from "next/image";
+import { User } from "lucide-react";
 import Link from "next/link";
+import HeroSection from "../_components/hero-section";
+import OrDivider from "../_components/or-divider";
+import SocialsAuth from "../_components/socials-auth";
 
 const Page = () => {
   return (
@@ -91,72 +93,20 @@ const Page = () => {
                   type="submit"
                   className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
                 >
-                  Create Account <User className="size-4 ml-1" />
+                  Create Account <User className="ml-1 size-4" />
                 </Button>
               </div>
             </div>
           </form>
 
-          <div className="relative my-8 w-full">
-            <hr />
-            <span className="text-muted-foreground absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-4 text-sm">
-              OR
-            </span>
-          </div>
+          <OrDivider />
 
-          <div className="mt-3 space-y-3">
-            <Button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-rose-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                </svg>
-              </span>
-              Sign in with Google
-            </Button>
-            <Button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <Facebook size={24} className="text-blue-600" />
-              </span>
-              Sign in with Facebook
-            </Button>
-          </div>
+          <SocialsAuth />
         </div>
       </section>
 
-      {/* Hero image section */}
-      <section className="relative hidden size-full overflow-hidden lg:block">
-        <div className="flex h-full items-center justify-center bg-gray-100">
-          <div className="relative h-full w-full">
-            <div className="absolute inset-0 bg-gradient-to-br from-yellow-400 to-yellow-600 opacity-70"></div>
-            <div
-              className="absolute inset-0"
-              style={{
-                backgroundImage:
-                  "url(\"data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23ffffff' fill-opacity='0.15' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='3'/%3E%3Ccircle cx='13' cy='13' r='3'/%3E%3C/g%3E%3C/svg%3E\")",
-                backgroundSize: "24px 24px",
-              }}
-            ></div>
-            <Image
-              className="absolute inset-0 h-full w-full object-cover mix-blend-overlay"
-              height={1080}
-              width={1920}
-              src="/placeholder.svg?height=1080&width=1920"
-              alt="Marketing analytics dashboard"
-            />
-          </div>
-        </div>
-      </section>
+      {/* Hero section */}
+      <HeroSection />
     </div>
   );
 };
