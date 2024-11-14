5246dd881c1bfd7bff1d9b5f3844c38f8aee8d00
xyugen <alex.arias.jorym@gmail.com>
2024-11-14 20:00:44 +0800

    feat: :page_facing_up: create login page template
    add button, input, and label components from shadcn

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,142 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Facebook, Mail } from "lucide-react";
+import Image from "next/image";
+
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className="grid min-h-screen lg:grid-cols-2">
+      <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+        <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+          <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
+            Login to your account!
+          </h2>
+          <p className="mt-2 text-sm text-gray-600">
+            Don&apos;t have an account?{" "}
+            <a
+              href="#"
+              title=""
+              className="font-semibold text-black transition-all duration-200 hover:underline"
+            >
+              Create a free account
+            </a>
+          </p>
+          <form action="#" method="POST" className="mt-8">
+            <div className="space-y-5">
+              <div>
+                <Label
+                  htmlFor="email"
+                  className="text-base font-medium text-gray-900"
+                >
+                  Email address
+                </Label>
+                <div className="mt-2">
+                  <Input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="email"
+                    placeholder="Email"
+                    id="email"
+                  ></Input>
+                </div>
+              </div>
 
-export default Page
+              <div>
+                <div className="flex items-center justify-between">
+                  <Label
+                    htmlFor="password"
+                    className="text-base font-medium text-gray-900"
+                  >
+                    Password
+                  </Label>
+                  <a
+                    href="#"
+                    title=""
+                    className="text-sm font-semibold text-black hover:underline"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <div className="mt-2">
+                  <Input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                  ></Input>
+                </div>
+              </div>
+              <div>
+                <Button
+                  type="submit"
+                  className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                >
+                  Get started <Mail className="ml-2" size={16} />
+                </Button>
+              </div>
+            </div>
+          </form>
+
+          <div className="relative my-8 w-full">
+            <hr />
+            <span className="text-muted-foreground absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-4 text-sm">
+              OR
+            </span>
+          </div>
+
+          <div className="mt-3 space-y-3">
+            <Button
+              type="button"
+              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+            >
+              <span className="mr-2 inline-block">
+                <svg
+                  className="h-6 w-6 text-rose-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
+                </svg>
+              </span>
+              Sign in with Google
+            </Button>
+            <Button
+              type="button"
+              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+            >
+              <span className="mr-2 inline-block">
+                <Facebook size={24} className="text-blue-600" />
+              </span>
+              Sign in with Facebook
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block">
+        <div className="flex h-full items-center justify-center bg-gray-100">
+          <div className="relative h-full w-full">
+            <div className="absolute inset-0 bg-gradient-to-br from-yellow-400 to-yellow-600 opacity-70"></div>
+            <div
+              className="absolute inset-0"
+              style={{
+                backgroundImage:
+                  "url(\"data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23ffffff' fill-opacity='0.15' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='3'/%3E%3Ccircle cx='13' cy='13' r='3'/%3E%3C/g%3E%3C/svg%3E\")",
+                backgroundSize: "24px 24px",
+              }}
+            ></div>
+            <Image
+              className="absolute inset-0 h-full w-full object-cover mix-blend-overlay"
+              height={1080}
+              width={1920}
+              src="/placeholder.svg?height=1080&width=1920"
+              alt="Marketing analytics dashboard"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
