9cf209d0759ce9dea2f7d1e49dfb74249252d71e
xyugen <alex.arias.jorym@gmail.com>
2024-11-29 21:46:08 +0800

    feat(dashboard): show best selling product on topcards

--- src/app/(app)/dashboard/_components/cards/top-card.tsx
+++ src/app/(app)/dashboard/_components/cards/top-card.tsx
@@ -3,13 +3,19 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/trpc/react";
-import { User } from "lucide-react";
+import { Package, User } from "lucide-react";
 import OverviewCard from "./overview-card";
 
 const TopCard = () => {
-  const { data, isLoading } = api.analytics.getTopSpendingCustomers.useQuery({
-    limit: 1,
-  });
+  const { data: topSpendingCustomer, isLoading } =
+    api.analytics.getTopSpendingCustomers.useQuery({
+      limit: 1,
+    });
+
+  const { data: bestSellingProducts } =
+    api.analytics.getBestSellingProducts.useQuery({
+      limit: 1,
+    });
 
   return (
     <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
@@ -25,8 +31,24 @@ const TopCard = () => {
         <OverviewCard
           title="Top Customer"
           icon={User}
-          data={`${data?.at(0)?.name}`}
-          subTitle={`₱${data?.at(0)?.outstandingBalance.toLocaleString()} total spend`}
+          data={`${topSpendingCustomer?.at(0)?.name}`}
+          subTitle={`₱${topSpendingCustomer?.at(0)?.outstandingBalance.toLocaleString()} total spend`}
+        />
+      )}
+      {isLoading ? (
+        <Card className="relative">
+          <Skeleton className="absolute right-6 top-6 size-10 rounded-full bg-primary p-2 text-primary-foreground" />
+          <CardHeader className="font-medium">Top Customer</CardHeader>
+          <CardContent className="space-y-2">
+            <Skeleton className="flex items-center gap-2 font-semibold" />
+          </CardContent>
+        </Card>
+      ) : (
+        <OverviewCard
+          title="Best Selling Product"
+          icon={Package}
+          data={`${bestSellingProducts?.at(0)?.productService.replace("(deleted)", "")}`}
+          subTitle={`₱${bestSellingProducts?.at(0)?.totalSales.toLocaleString()} total sales`}
         />
       )}
       {isLoading ? (
@@ -41,24 +63,8 @@ const TopCard = () => {
         <OverviewCard
           title="Top Customer"
           icon={User}
-          data={`${data?.at(0)?.name}`}
-          subTitle={`₱${data?.at(0)?.outstandingBalance.toLocaleString()} total spend`}
-        />
-      )}
-      {isLoading ? (
-        <Card className="relative">
-          <Skeleton className="absolute right-6 top-6 size-10 rounded-full bg-primary p-2 text-primary-foreground" />
-          <CardHeader className="font-medium">Top Customer</CardHeader>
-          <CardContent className="space-y-2">
-            <Skeleton className="flex items-center gap-2 font-semibold" />
-          </CardContent>
-        </Card>
-      ) : (
-        <OverviewCard
-          title="Top Customer"
-          icon={User}
-          data={`${data?.at(0)?.name}`}
-          subTitle={`₱${data?.at(0)?.outstandingBalance.toLocaleString()} total spend`}
+          data={`${topSpendingCustomer?.at(0)?.name}`}
+          subTitle={`₱${topSpendingCustomer?.at(0)?.outstandingBalance.toLocaleString()} total spend`}
         />
       )}
     </div>
