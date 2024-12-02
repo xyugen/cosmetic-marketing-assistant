d67acf28e64657410279f3562bbd79f605c17657
xyugen <alex.arias.jorym@gmail.com>
2024-12-02 21:48:04 +0800

    style: :lipstick: use css theme variables for auth pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,15 +11,15 @@ const Page = () => {
   return (
     <section className="flex flex-col items-center justify-center overflow-y-scroll p-6 lg:p-12">
       <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
+        <h2 className="text-3xl font-bold leading-tight text-foreground sm:text-4xl">
           Login to your account!
         </h2>
-        <p className="mt-2 text-sm text-gray-600">
+        <p className="mt-2 text-sm text-muted-foreground">
           Don&apos;t have an account?{" "}
           <Link
             href={PageRoutes.SIGNUP}
             title="Sign Up"
-            className="font-semibold text-black transition-all duration-200 hover:underline"
+            className="font-semibold text-foreground transition-all duration-200 hover:underline"
           >
             Create a free account
           </Link>
